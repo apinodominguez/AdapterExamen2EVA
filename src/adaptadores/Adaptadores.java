39e69048dd26bc5dd10f3809db59f8c8e0a653b6
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2019-03-07 09:44:33 +0100

    Ejercicio Adapter Examen 2EVA. Terminado

--- src/adaptadores/Adaptadores.java
+++ src/adaptadores/Adaptadores.java
@@ -9,6 +9,10 @@ public class Adaptadores {
         player.play("file.mp4");
         player = new FormatAdapter(new VLC());
         player.play("file.avi");
+        MediaDivX reproductor = new DVD();
+        reproductor.playFilm("file.dvd");
+        reproductor = new FormatAdapter2(new VLC());
+        reproductor.playFilm("file.avi");
     }
 
     private static class FormatAdapter implements MediaPlayer {
@@ -24,5 +28,20 @@ public class Adaptadores {
             System.out.print("Using Adapter --> ");
             media.playFile(filename);
         }
+    }
+    private static class FormatAdapter2 implements MediaDivX{
+        
+        private MediaPackage video;
+        
+        public FormatAdapter2 (MediaPackage v){
+            video = v;
+        }
+        
+        @Override
+        public void playFilm(String filename) {
+            System.out.print("Using Adapter2 --> ");
+            video.playFile(filename);
+        }
+        
     }
 }
---
+++ src/adaptadores/DVD.java
@@ -0,0 +1,10 @@
+package adaptadores;
+
+public class DVD implements MediaDivX {
+
+    @Override
+    public void playFilm(String filename) {
+        System.out.println("Playing DVD File " + filename);
+    }
+    
+}
